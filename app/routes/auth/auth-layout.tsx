74a6c66b2e080f5d83d0b2f004f916a5143dee33
kanika4000 <157558757+kanika4000@users.noreply.github.com>
2025-09-16 19:41:24 +0530

    Update auth-layout.tsx

--- app/routes/auth/auth-layout.tsx
+++ app/routes/auth/auth-layout.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router';
 const AuthLayout = () => {
 return (
-    <div className='wfrontend/app/routes-full h-screen flex items-center justify-center'>
         <Outlet />
-    </div>
 );
 };
    
